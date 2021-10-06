2a2f4a087d77e041897699e354c7cd6a9956cf07
ATH\nmavrofo <nikos.mavroforos@intrasoft-intl.com>
2021-10-06 15:50:30 +0300

    Minor Change in for spacing

--- app/core/migrations/0001_initial.py
+++ app/core/migrations/0001_initial.py
@@ -21,24 +21,41 @@ class Migration(migrations.Migration):
                 ('last_login', models.DateTimeField(blank=True, null=True,
                                                     verbose_name='last login')),
                 ('is_superuser', models.BooleanField(default=False,
-                                                     help_text='Designates that this user has all permissions without explicitly assigning them.',
-                                                     verbose_name='superuser status')),
+                                                     help_text='Designates '
+                                                               'that this '
+                                                               'user has all '
+                                                               'permissions '
+                                                               'without '
+                                                               'explicitly '
+                                                               'assigning '
+                                                               'them.',
+                                                     verbose_name='superuser '
+                                                                  'status')),
                 ('email', models.EmailField(max_length=255, unique=True)),
                 ('name', models.CharField(max_length=255)),
                 ('is_active', models.BooleanField(default=True)),
                 ('is_staff', models.BooleanField(default=False)),
                 ('groups', models.ManyToManyField(blank=True,
-                                                  help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+                                                  help_text='The groups this '
+                                                            'user belongs '
+                                                            'to. A user will '
+                                                            'get all '
+                                                            'permissions '
+                                                            'granted to each '
+                                                            'of their '
+                                                            'groups.',
                                                   related_name='user_set',
                                                   related_query_name='user',
                                                   to='auth.Group',
                                                   verbose_name='groups')),
-                ('user_permissions', models.ManyToManyField(blank=True,
-                                                            help_text='Specific permissions for this user.',
-                                                            related_name='user_set',
-                                                            related_query_name='user',
-                                                            to='auth.Permission',
-                                                            verbose_name='user permissions')),
+                ('user_permissions', models.ManyToManyField(
+                    blank=True,
+                    help_text='Specific permissions'
+                 ' for this user.',
+                    related_name='user_set',
+                    related_query_name='user',
+                    to='auth.Permission',
+                    verbose_name='user permissions')),
             ],
             options={
                 'abstract': False,
